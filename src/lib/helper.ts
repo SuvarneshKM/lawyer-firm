5653946a2d759a279da221b554500a993ac4a61d
SuvarneshKM <suvarnesh1729@gmail.com>
2023-12-14 20:02:54 +0530

    fix: BASE_URL

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:4000";
+// const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://my-json-server.typicode.com/SuvarneshKM/lawyer-firm";
 
 export const getAllLawyers = () => fetch(`${BASE_URL}/lawyers`);
